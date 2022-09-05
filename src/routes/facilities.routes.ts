1ba81b0100d276d3fefda71eec812690852e1011
Jestin Mounts <jkmounts@gmail.com>
2022-09-05 10:12:39 -0400

    facility get route

--- src/routes/facilities.routes.ts
+++ src/routes/facilities.routes.ts
@@ -3,6 +3,12 @@ import FacilityService from '../services/facility.service';
 
 const router = Router();
 
+router.get('/', async (req, res) => {
+  const facilityService = new FacilityService;
+  const facilites = await facilityService.all();
+  res.json(facilites);
+});
+
 router.post('/', async(req, res) => {
   const facilityService = new FacilityService();
   const result = await facilityService.add(req.body);
